8f329497ba7e548b424f9a759d47daf1b3390e77
murlinmurlo <masha.burdugova@mail.ru>
2024-11-30 22:09:36 +0300

    task 6.3

---
+++ task_6/3/intrusive_ptr.h
@@ -0,0 +1,132 @@
+#ifndef INTRUSIVE_PTR_H
+#define INTRUSIVE_PTR_H
+
+#include <utility>
+#include <iostream>
+
+template<typename T>
+class TRefCounter {
+public:
+    TRefCounter() : ref_count(0) {}
+
+    void NewRef() {
+        ++ref_count;
+    }
+
+    void Release() {
+        if (--ref_count == 0) {
+            delete static_cast<T*>(this);
+        }
+    }
+
+    size_t RefCount() const {
+        return ref_count;
+    }
+
+private:
+    size_t ref_count;
+};
+
+template<typename T>
+class TPtr {
+public:
+    TPtr(T* ptr = nullptr) : ptr(ptr) {
+        if (ptr) {
+            ptr->NewRef();
+        }
+    }
+
+    TPtr(const TPtr& other) : ptr(other.ptr) {
+        if (ptr) {
+            ptr->NewRef();
+        }
+    }
+
+    TPtr(TPtr&& other) noexcept : ptr(other.ptr) {
+        other.ptr = nullptr;
+    }
+
+    ~TPtr() {
+        Reset();
+    }
+
+    TPtr& operator=(const TPtr& other) {
+        if (this != &other) {
+            Reset();
+            ptr = other.ptr;
+            if (ptr) {
+                ptr->NewRef();
+            }
+        }
+        return *this;
+    }
+
+    TPtr& operator=(TPtr&& other) noexcept {
+        if (this != &other) {
+            Reset();
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+    explicit operator bool() const {
+        return ptr != nullptr;
+    }
+
+    bool operator==(const TPtr& other) const {
+        return ptr == other.ptr;
+    }
+
+    bool operator!=(const TPtr& other) const {
+        return ptr != other.ptr;
+    }
+
+    T* Get() const {
+        return ptr;
+    }
+
+    void Reset(T* ptr_new = nullptr) {
+        if (ptr) {
+            ptr->Release();
+        }
+        ptr = ptr_new;
+        if (ptr) {
+            ptr->NewRef();
+        }
+    }
+
+    void Reset(const TPtr& ptr_new) {
+        Reset(ptr_new.Get());
+    }
+
+    T* Release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+protected:
+    T* ptr;
+};
+
+template<typename T>
+class TIntrusivePtr : public TPtr<T> {
+public:
+    using TPtr<T>::TPtr;
+};
+
+template<typename T, typename... Args>
+TIntrusivePtr<T> MakeIntrusive(Args&&... args) {
+    return TIntrusivePtr<T>(new T(std::forward<Args>(args)...));
+}
+
+#endif
---
+++ task_6/3/main.cpp
@@ -0,0 +1,23 @@
+#include "intrusive_ptr.h"
+
+class TDoc : public TRefCounter<TDoc> {
+public:
+    TDoc() {}
+
+    ~TDoc() {}
+};
+
+int main() {
+    TIntrusivePtr<TDoc> ptr = nullptr;
+    ptr = MakeIntrusive<TDoc>();
+    TIntrusivePtr<TDoc> ptr2 = ptr;
+    TIntrusivePtr<TDoc> ptr3 = MakeIntrusive<TDoc>();
+    
+    ptr3.Reset(ptr2);
+    ptr3.Reset();
+    ptr3.Reset(std::move(ptr2));
+
+    static_assert(sizeof(TDoc*) == sizeof(ptr));
+
+    return 0;
+}
